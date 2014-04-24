363ba298aea2382dbe7146f744bbc3baf8caf96d
AndyLampert <lamperta@gmail.com>
2014-04-24 14:31:15 -0600

    set up filter

--- main.js
+++ main.js
@@ -12,12 +12,28 @@ $(document).on('ready', function() {
 		// create new nodes
 		$('body').append(makeGameList);
 		makeGameList.append(makeGameName);
-		makeGameName.append(makeReason);
+		makeGameList.append(makeReason);
 
 		// add the data from the inputs
 		makeGameName.append(inputGame);
 		makeReason.append(inputReason);
 
-		//
-	})
+	});
+	// when you click on a game title, hide all other games
+	// delegated event handler
+	$(document).on('click', '.game-name', function(){
+		var currentGameTitle = $(this).text();
+		// logs the text from the .game-name element that you click on
+		console.log(currentGameTitle);
+		var filterGames = $('.game-list').toArray().filter(function(item){
+			var checkGame = $(item).find('.game-name').text();
+			// logs ALL .game-name values 
+			console.log(checkGame);
+			if(currentGameTitle !== checkGame) {
+				return true;
+			}
+				return false;
+		});
+		console.log(filterGames);
+	});
 });
