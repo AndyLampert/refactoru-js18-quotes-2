77183bb32796f5703bdb3e53e7c1df5ee689f1e6
AndyLampert <lamperta@gmail.com>
2014-04-24 14:03:21 -0600

    set up event handlers

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(document).on('ready', function() {
-  
+	// create variables
+	// click handler for submit
+	$(document).on('click', '#submit', function(){
+		// add the data from the inputs
+		var inputGame = $('.game-input').val();
+		var inputReason = $('.reason-input').val();
+		console.log(inputReason);
+		console.log(inputGame);
+		$('.reason-input').val();
+	})
 });
